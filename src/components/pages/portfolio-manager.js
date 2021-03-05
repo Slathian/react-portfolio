13d4701f3bab7ec9873371f0eeab2f7dd9b782a6
Slathian <joshua@angelo.net>
2021-03-04 16:12:32 -0800

    computer cleanup

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -3,17 +3,44 @@ import axios from 'axios'
 
 export default function PortfolioManager() {
 
-    const [data, changeData] = useState("");
+    const [portfolioItems, changePortfolioItems] = useState("");
+
+    const getPortfolioItems = () => {
+        axios
+        .get("https://joshuaangelo.devcamp.space/portfolio/portfolio_items", {withCredentials: true})
+        .then(response => {
+            const apiCallData = response.data.portfolio_items
+            console.log(apiCallData)
+            changePortfolioItems([{this: "testing data"}])
+            console.log("state section: ", portfolioItems)
+        }).catch(err => {
+            console.log("error in response", err)
+        });
+    };
+
+    handleData = () => {
+        
+    }
+
+    useEffect( () => {
+        getPortfolioItems()
+        console.log(portfolioItems)
+    }, [])
 
     return (
         <div>
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
-                    <h1>Portfolio form....</h1>
+                    <form>
+                        <input>
+                        </input>
+                        
+                    </form>
                 </div>
 
                 <div className="right-column">
                     <h1>Portfolio sidebar....</h1>
+                    
                 </div>
             </div>
         </div>
