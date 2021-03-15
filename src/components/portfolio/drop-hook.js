a41eed5df65e52ad22a967635d3839a33a79211b
Donner <joshua@angelo.net>
2021-03-15 10:26:21 -0700

    Working Drag and drop

--- src/components/portfolio/drop-hook.js
+++ src/components/portfolio/drop-hook.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useDropzone} from 'react-dropzone';
 
 export default function Basic(props) {
-  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+
+  const [uploadedImage, setImage] = useState([]);
+  const [imageDropped, setBool] = useState(false);
+  const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone({
+    accept: "image/*",
+    onDrop: (acceptedFiles) => {
+      setBool(true);
+      setImage(
+        acceptedFiles.map((upFile) => Object.assign(upFile,{
+          preview: URL.createObjectURL(upFile)
+        }))
+      )
+    } 
+  });
   
   const files = acceptedFiles.map(file => (
     <li key={file.path}>
@@ -10,12 +23,22 @@ export default function Basic(props) {
     </li>
   ));
 
+  
+
   return (
       <section className="drop-container">
         <div {...getRootProps({className: 'dropzone'})}>
           <input {...getInputProps()} />
-          <p>{props.name}</p>
-          <ul>{files}</ul>
+          
+          {imageDropped ? null : <p>Drop {props.name} or select</p>}
+
+          {uploadedImage.map((upFile) =>{
+            return (
+              <div className="preview-wrapper" key={"preview", props.name}>
+                <img src={upFile.preview} style={{width:"200px", height: "auto"}} alt="preview" />
+              </div>
+            )
+          })}
         </div>
       </section>
   );
